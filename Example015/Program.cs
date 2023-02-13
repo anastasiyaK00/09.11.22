c6de1b601e7fd9c9f7568c199eecd542e76e0405
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-13 21:41:27 +0300

    015-16

---
+++ Example015/Program.cs
@@ -0,0 +1,30 @@
+﻿// Внести изменения в текст
+Console.Write("add text:");
+string text = Console.ReadLine();
+
+string Replace ( string text, char Old, char New)
+{
+    string result = string.Empty;
+    int Length = text.Length;
+    for (int i = 0; i<Length; i++)
+    {
+        if (text[i] == Old) 
+        {
+            result=result+ New;
+        }
+        else 
+        {
+           result= result + text[i];
+        }
+    }
+    return result;
+}
+
+
+string newText = Replace(text,' ', '|');
+Console.WriteLine(" ");
+string newText1 = Replace(text,'k', 'K');
+Console.WriteLine(" ");
+string newText2 = Replace(text,'o', 'O');
+Console.WriteLine(" ");
+Console.WriteLine(newText2);
---
+++ Example016/Program.cs
@@ -0,0 +1,38 @@
+﻿// сортировка массива
+
+int[] arr = { 7, 4, 3, 5, 2, 9, 23, 6 };
+
+void PrintArray(int[] array) //Метод, печатающий наш массив
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine(" ");
+}
+
+void Selection(int[] array)
+{
+    for (int i = 0; i < array.Length -1 ; i++)
+    {
+        int minposition = i; // та позиция на которую смотрим
+
+        for (int j = i + 1; j < array.Length; j++)//+1 тк с не отсортированного начинаем
+        {
+            if (array[j] > array[minposition])
+            {
+                minposition = j;
+            }
+        }
+
+        int temporary = array[i];// меняем позицию минимального с той, которую нашли 
+        array[i] = array[minposition];//обмен двух переменных местами
+        arr[minposition] = temporary; //кладем в мин тот что временный
+    }
+}
+
+
+PrintArray(arr);
+Selection(arr);
+PrintArray(arr);
