d2105fe4b69281c60458b4b63b78ca2dcff1a1c4
Anastasiya <anastasiyak96.96@mail.ru>
2023-02-13 20:20:58 +0300

    012

---
+++ Example012/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+string metod4 (int count, string text)
+{
+    int i=0;
+    string result = String.Empty;
+while (i<count)
+{
+    result = result+text;
+    i++;
+}
+return result;
+}
+
+string res = metod4(10,"z");
+Console.WriteLine(res);
